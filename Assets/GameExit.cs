b88210ea8996807524b8d46eb74e69ee9e889ccf
Aldo Salgado <asalgado@interiorvista.com>
2018-09-22 21:09:28 +0200

    Reto 3

---
+++ Assets/GameExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+
+    /**
+     * Por alguna razon no se detecta 
+     * 
+     */
+    public void SalirJuego () {
+            Debug.Log("Saliendo del juego...");
+            Application.Quit();
+    }
+
+}
