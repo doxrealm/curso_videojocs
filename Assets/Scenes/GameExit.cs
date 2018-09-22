30db9907ea450e2551c462854cb379c204448600
Aldo Salgado <asalgado@interiorvista.com>
2018-09-22 20:36:20 +0200

    Reto 1

---
+++ Assets/Scenes/GameExit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+
+    /**
+     * Por alguna razon no se detecta 
+     * 
+     */
+    public void SalirJuego () {
+            Debug.Log("Saliendo del juego...");
+    }
+
+}
